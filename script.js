f990400e58bfc8978102af737989d03fb2846225
jim113453 <31411325+jim@users.noreply.github.com>
2018-01-05 19:02:55 -0500

    worked on javascript more, making progress

--- script.js
+++ script.js
@@ -3,23 +3,47 @@ var generatedArr = [];
 var playerArr = [];
 var counter = 0;
 
-$("button").click(function() {
+$("button").click(play);
+
+function play() {
 	var randInt = Math.floor(Math.random()*4);
-	var randColor = $(".board").eq(randInt);
-	randColor.fadeOut(500).fadeIn(400); //from all the matched elements, pick a random one
-	generatedArr.push(randColor.css("backgroundColor"));
+	var randDiv = $(".grid_container").children().eq(randInt);
+	var updatedClass = randDiv.attr("class");
+	generatedArr.push(updatedClass);
+	randDiv.fadeOut(500).fadeIn(400);
 	$("button").hide();
 	counter++;
 	$("h1").text(`Round ${counter}! Click the flashed colors!`);
-	$(".board").click(function() {
-		playerArr.push($(this).css("backgroundColor"))});
-});
-// while (counter < 15){ 				//fixing this still
-// 	if (playerArr == generatedArr){
-// 		counter++;
-// 		$("h1").text(`Round ${counter}!`);
-// 	}
-// 	else {
-// 		break;
-// 	}
+	playerArr = [];
+	$(".grid_container").children().click(function() {
+		playerArr.push(this.className);
+		if (generatedArr.toString() === playerArr.toString() ) {
+			alert(`Success! Move on to level ${counter+1}!`);
+			play();
+		}
+		else {
+			alert('You have lost!');
+		}
+	});
+}
+
+
+
+
+// function repeatPlay(){
+// 	play();
 // }
+
+// document.querySelector("button").addEventListener("click", function(){
+// 	var randInt = Math.floor(Math.random()*4);
+// 	var allDivs = document.querySelectorAll(".grid_container.children");
+// 	var randDiv = allDivs[randInt];
+// 	$(randDiv).fadeOut(500).fadeIn(400);
+// 	generatedArr.push(this.className);
+// 	document.querySelector("button").style.display = "none";
+// 	counter++;
+// 	document.querySelector("h1").textContent = `Round ${counter}! Click the flashed colors!`;
+// 	document.querySelectorAll(".grid_container.children").addEventListener("click",function (){
+// 		playerArr.push(this.className);
+// 	})
+// });
