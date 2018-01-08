6bf2c317679f856b89b3f724569961de0ca808bd
jim113453 <31411325+jim@users.noreply.github.com>
2018-01-08 13:55:59 -0500

    refactoring code

--- script.js
+++ script.js
@@ -1,33 +1,74 @@
 
 var generatedArr = [];
 var playerArr = [];
-var counter = 0;
+var counter = 1;
+var gameboard = $(".grid_container").children();
 
 $("button").click(play);
 
-function play() {
-	var randInt = Math.floor(Math.random()*4);
-	var randDiv = $(".grid_container").children().eq(randInt);
-	var updatedClass = randDiv.attr("class");
-	generatedArr.push(updatedClass);
-	randDiv.fadeOut(500).fadeIn(400);
+// function play() {
+// 	for (var i = 0; i < counter; i++){
+// 		var randDiv = gameboard.eq(Math.floor(Math.random()*4));
+// 		//console.log({generatedArr})
+// 		generatedArr.push(randDiv.attr("class"));
+// 		.fadeOut(500).fadeIn(400).delay(1900);
+// 	}
+// 	$("button").hide();
+// 	$("h1").text(`Round ${counter}! Click the flashed colors!`);
+// 	counter++;
+// 	playerArr = [];
+// 	$(".grid_container").children().click(function() {
+// 		playerArr.push(this.className);
+// 		if (playerArr.length === (counter - 1)){
+// 			checkIfRight();
+// 		}
+// 	});
+// }
+
+function play(){
+	var random = Math.floor(Math.random()*4);
+	var randDiv = gameboard.eq(random)[0];
+	randDiv = $(randDiv);
+	// previous colors
+	generatedArr.push(randDiv);
+	// players choices
+	// console.log(this);
+	// playerArr.push(this);
+	for(var i = 0; i < generatedArr.length; i++){
+		generatedArr[i].fadeOut(500).fadeIn(400);
+		generatedArr[i].click(function(){
+			playerArr.push($(this));
+			if(playerArr.length === generatedArr.length){
+				checkIfRight();
+			}
+		})
+	}
+
 	$("button").hide();
-	counter++;
 	$("h1").text(`Round ${counter}! Click the flashed colors!`);
-	playerArr = [];
-	$(".grid_container").children().click(function() {
-		playerArr.push(this.className);
-		if (generatedArr.toString() === playerArr.toString() ) {
-			alert(`Success! Move on to level ${counter+1}!`);
-			play();
-		}
-		else {
-			alert('You have lost!');
-		}
-	});
+
 }
 
+// function checkIfRight(){
+// 	if (generatedArr.toString() === playerArr.toString() ) {
+// 		alert(`Success! Move on to level ${counter}!`);
+// 		play();
+// 	}
+// 	else {
+// 		alert('You have lost!');
+// 	}
+// }
 
+function checkIfRight(){
+	console.log(generatedArr[0][0].className);
+	console.log(playerArr[0]);
+	if(generatedArr[0][0].className === playerArr[0][0].className){
+			alert(`Success! Move on to level ${counter}!`);
+			play();
+		} else {
+			alert('You have lost!');
+		}
+}
 
 
 // function repeatPlay(){
