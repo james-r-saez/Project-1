8848bdf07a071e2004b495e9ed69d9feb344fbd5
jim113453 <31411325+jim@users.noreply.github.com>
2018-01-09 09:53:18 -0500

    prettify pass

--- script.js
+++ script.js
@@ -1,4 +1,3 @@
-
 var generatedArr = [];
 var playerArr = [];
 var counter = 1;
@@ -6,49 +5,51 @@ var gameboard = $(".grid_container").children();
 
 var clickNumber = 0;
 
-gameboard.click(function(){
-	playerArr.push($(this));
-	clickNumber++;
-	checkIfRight();
+gameboard.click(function() {
+  playerArr.push($(this));
+  clickNumber++;
+  checkIfRight();
 });
 
 $("button").click(play);
 
-function play(){
-	//playerArr = [];
-	$("button").hide();
-	var random = Math.floor(Math.random()*4);
-	var randDiv = gameboard.eq(random)[0];
-	var randDiv = $(randDiv);
-	generatedArr.push(randDiv);
-	console.log(generatedArr);
-	for(var i = 0; i < generatedArr.length; i++){
-		generatedArr[i].delay(400).fadeOut(600).fadeIn(500);
-	}
-	$("h1").text(`Level ${counter}! Click the flashed colors!`);
+function play() {
+  //playerArr = [];
+  $("button").hide();
+  var random = Math.floor(Math.random() * 4);
+  var randDiv = gameboard.eq(random)[0];
+  var randDiv = $(randDiv);
+  generatedArr.push(randDiv);
+  console.log(generatedArr);
+  for (var i = 0; i < generatedArr.length; i++) {
+    generatedArr[i]
+      .delay(400)
+      .fadeOut(600)
+      .fadeIn(500);
+  }
+  $("h1").text(`Level ${counter}! Click the flashed colors!`);
 }
 
-function checkIfRight(){
-	if (playerArr.length === generatedArr.length){
-		for (var i=0; i < playerArr.length; i++){
-			for (var j=0; j < generatedArr.length; j++){
-				console.log("this is genarr j", generatedArr[j])
-				console.log("this is playerarr j", playerArr[i])
-				if (playerArr[i][i] ===  generatedArr[j][j]) {
-					win();
-				}
-				else {
-						alert('You have lost!');
-						location.reload();
-				}
-			}
-		}
-	}
+function checkIfRight() {
+  if (playerArr.length === generatedArr.length) {
+    for (var i = 0; i < playerArr.length; i++) {
+      for (var j = 0; j < generatedArr.length; j++) {
+        console.log("this is genarr j", generatedArr[j]);
+        console.log("this is playerarr j", playerArr[i]);
+        if (playerArr[i][i] === generatedArr[j][j]) {
+          win();
+        } else {
+          alert("You have lost!");
+          location.reload();
+        }
+      }
+    }
+  }
 }
 
 function win() {
-	counter++;
-	alert(`Success! Move on to level ${counter}!`);
-	playerArr = [];
-	play();
+  counter++;
+  alert(`Success! Move on to level ${counter}!`);
+  playerArr = [];
+  play();
 }
