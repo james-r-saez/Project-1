ee70ae6deb661cbdd7196f951a8e188b2acd1088
jim113453 <31411325+jim@users.noreply.github.com>
2018-01-08 19:41:21 -0500

    js edit

--- script.js
+++ script.js
@@ -4,45 +4,52 @@ var playerArr = [];
 var counter = 1;
 var gameboard = $(".grid_container").children();
 
-function play(){
-	//playerArr = [];
-	var random = Math.floor(Math.random()*4);
-	var randDiv = gameboard.eq(random)[0];
-	randDiv = $(randDiv);
-	generatedArr.push(randDiv);
-	for(var i = 0; i < generatedArr.length; i++){
-		generatedArr[i].delay(500).fadeOut(600).fadeIn(500).delay(500);
-		generatedArr[i].click(function(){
-			playerArr.push($(this));
-			console.log("The gen length is " + generatedArr.length);
-			console.log("The player length is " + playerArr.length);
-			if (playerArr.length === generatedArr.length){
-				console.log('this is playerarr length:', playerArr.length);
-				console.log('this is generatedArr length:', generatedArr.length);
-				checkIfRight();
-			}
-		})
-	}
-	$("button").hide();
-	$("h1").text(`Level ${counter}! Click the flashed colors!`);
-}
+var clickNumber = 0;
+
+gameboard.click(function(){
+	playerArr.push($(this));
+	clickNumber++;
+	checkIfRight();
+});
 
 $("button").click(play);
 
+function play(){
+	//playerArr = [];
+	$("button").hide();
+	var random = Math.floor(Math.random()*4);
+	var randDiv = gameboard.eq(random)[0];
+	var randDiv = $(randDiv);
+	generatedArr.push(randDiv);
+	for(var i = 0; i < generatedArr.length; i++){
+		generatedArr[i].delay(400).fadeOut(600).fadeIn(500).delay(400);
+	}
+	$("h1").text(`Level ${counter}! Click the flashed colors!`);
+}
+
 function checkIfRight(){
-	for (var i=0; i < playerArr.length; i++){
-	// console.log(generatedArr[0][0].className);
-	 console.log(playerArr[0]);
-		if(generatedArr[i][0] === playerArr[i][0]){
-			 counter++;
-			 alert(`Success! Move on to level ${counter}!`);
-			 playerArr = [];
-			 console.log('this is playerArr:', playerArr);
-			 play();
-		}
-		else {
-			alert('You have lost!');
-			location.reload();
+	if (playerArr.length === generatedArr.length){
+			// console.log('this is playerarr length:', playerArr.length);
+			// console.log('this is generatedArr length:', generatedArr.length);
+		for (var i=0; i < playerArr.length; i++){
+			for (var j=0; j < generatedArr.length; j++){
+				console.log("this is genarr j", generatedArr[j])
+				console.log("this is playerarr j", playerArr[i])
+				if (playerArr[i].className ===  generatedArr[j].className) {
+					win();
+				}
+				else {
+						alert('You have lost!');
+						// location.reload();
+				}
+			}
 		}
 	}
+}
+
+function win() {
+	counter++;
+	alert(`Success! Move on to level ${counter}!`);
+	playerArr = [];
+	play();
 }
